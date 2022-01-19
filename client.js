f23db607082fcf49b3a9d33e84635718ae34e3ad
Brendan Woo <woo.brendan@gmail.com>
2022-01-19 18:59:23 +0000

    moved connect function from play.js to newly created client.js. refactored to use es6 shorthand import/export

---
+++ client.js
@@ -0,0 +1,16 @@
+const net = require('net');
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log(`${data}`);
+  })
+  return conn;
+};
+
+module.exports = {
+  connect
+}
--- play.js
+++ play.js
@@ -1,17 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  conn.setEncoding('utf8');
-  conn.on('data', (data) => {
-    console.log(`${data}`);
-  })
-  return conn;
-};
-
+const {connect} = require('./client');
 
 console.log('Connection ...');
 connect();
