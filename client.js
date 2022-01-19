ccd14d744c987c2e5041720da127a520667da0b8
Brendan Woo <woo.brendan@gmail.com>
2022-01-19 19:06:06 +0000

    added on connect function, to log confirmation of connection as well as pass name on to the server

--- client.js
+++ client.js
@@ -5,8 +5,13 @@ const connect = function() {
     port: 50541
   });
   conn.setEncoding('utf8');
+
   conn.on('data', (data) => {
     console.log(`${data}`);
+  })
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+    conn.write('Name: WOO');
   })
   return conn;
 };
