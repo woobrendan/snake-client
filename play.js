9a774e041aa75fa8df7e1812714af209d843860a
Brendan Woo <woo.brendan@gmail.com>
2022-01-19 18:50:01 +0000

    initial add of play file for snake client. allows for connection to remove server (not the livestream), and allows receive dead message on exit/disconnect from idle

---
+++ play.js
@@ -0,0 +1,17 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  conn.setEncoding('utf8');
+  conn.on('data', (data) => {
+    console.log(`${data}`);
+  })
+  return conn;
+};
+
+
+console.log('Connection ...');
+connect();
