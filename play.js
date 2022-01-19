766386b4990856e7171a2ccdae6e2a08b724e506
Brendan Woo <woo.brendan@gmail.com>
2022-01-19 19:24:40 +0000

    added setupInput function to allow for input to be received from the user. added function to allow for ctrl c termination

--- play.js
+++ play.js
@@ -1,4 +1,20 @@
 const {connect} = require('./client');
 
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
 console.log('Connection ...');
 connect();
+setupInput();
