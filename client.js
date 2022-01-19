f4b4b811b695fe83141e3075045497e8da2c5771
Brendan Woo <woo.brendan@gmail.com>
2022-01-19 19:14:46 +0000

    added move up command on connection, then commented out that line

--- client.js
+++ client.js
@@ -12,6 +12,7 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: WOO');
+    // conn.write('Move: up');
   })
   return conn;
 };
