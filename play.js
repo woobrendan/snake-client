105eea2e1e6f4baff6cfb6d244405a57ff95e347
Brendan Woo <woo.brendan@gmail.com>
2022-01-19 19:31:20 +0000

    added input file, moved the setupinput function and handleuser func to that file. added require statement on play file

--- play.js
+++ play.js
@@ -1,20 +1,8 @@
 const {connect} = require('./client');
+const {setupInput} = require('./input');
 
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-}
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
 
 console.log('Connection ...');
 connect();
+
 setupInput();
